46bdae9ef52ccb2557e753ff9b7720d614a8f056
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-12-03 18:51:57 +0500

    add Blog/BlogPost pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
-import About from "./about"
+// import About from "./about"
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
@@ -10,7 +12,26 @@ const Blog = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     {/* <About /> */}
-    <div className="title">Blog Page</div>
+    <div className={styles.blogs}>
+          <div className='blogItem'>
+            <Link href='/blogpost/First Blog'>
+              <h3>First Blog</h3>
+            </Link>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+          <div className='blogItem'>
+            <Link href='/blogpost/Second Blog'>
+              <h3>Second Blog</h3>
+            </Link>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+          <div className='blogItem'>
+            <Link href='/blogpost/Third Blog'>
+              <h3>Third Blog</h3>
+            </Link>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+        </div>
     </>
   )
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -2,10 +2,40 @@ import { useRouter } from "next/router";
 
 const Post = () => {
   const router = useRouter();
-  console.log(router.query)
+  console.log(router.query);
   const { slug } = router.query;
 
-  return <p>Post: {slug}</p>;
+  return (
+    <>
+      <style jsx>
+        {`
+          .center {
+            justify-content: center;
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            margin-top: 30px !important;
+            max-width: 50%;
+            margin: auto;
+          }
+
+          h3 {
+            margin: 0px;
+          }
+
+          .divider {
+            width: 50%;
+          }
+        `}
+      </style>
+
+      <div className="center">
+        <h3>{slug}</h3>
+        <hr className="divider" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, corporis ducimus veniam aliquid eaque nam hic nobis a option</p>
+      </div>
+    </>
+  );
 };
 
 export default Post;
